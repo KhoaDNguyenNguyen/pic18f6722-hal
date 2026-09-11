5cb55f588f958a46cedb177cdc52b0f990195a98
Dang-Khoa N. Nguyen <khoadnguyennguyen@gmail.com>
2026-09-11 14:56:17 +0700

    refactor: add global include header
    Consolidate all component headers into a single include file for simplified application development.

---
+++ inc/all.h
@@ -0,0 +1,24 @@
+#ifndef ALL_H
+#define ALL_H
+
+#include <xc.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "config.h"
+#include "hardware.h"
+#include "mbi5026.h"
+#include "lcd.h"
+#include "glcd.h"
+#include "led7seg.h"
+#include "led32.h"
+#include "led_matrix.h"
+#include "keypad.h"
+#include "button.h"
+#include "rotary_encoder.h"
+#include "buzzer.h"
+#include "timer.h"
+#include "counter.h"
+#include "test_suite.h"
+
+#endif
