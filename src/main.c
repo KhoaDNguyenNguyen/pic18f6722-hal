1a8c1a7542e93ea8b6f2d207ba45f30341b4a2f9
Dang-Khoa N. Nguyen <khoadnguyennguyen@gmail.com>
2026-09-13 15:00:19 +0700

    feat: implement main application using high-level APIs
    Demonstrate non-blocking LED animations, hardware polling, and screen updates in the main loop.

--- src/main.c
+++ src/main.c
@@ -1,10 +1,18 @@
-#include "config.h"
-#include "mbi5026.h"
+#include "all.h"
 
-// make flash MAIN_SRC=week3/ex_418.c
 int main(void) {
-    MBI_Init();
-    while (1) {
+    Easy_System_Init(); 
+
+
+    while(1) {
+    
+        Easy_System_Run();
+        uint8_t key = Easy_Keypad_GetKey();
+        Easy_LCD_PrintFormat(0, 0, "key: %d is pressed", key);
+        Easy_GLCD_PrintFormat(0, 0, "key: %d is pressed", key);
+        Easy_LED7_PrintNum(key);
+        for(int i = 0; i<key; i++) Easy_LED32_EnableBlink(key, 1000);
+        Easy_Matrix_Print("key", 1);
 
     }
     return 0;
